8b7f4349fade89d175741ce15b1db1f549547c90
kamchuen12 <47215841+kamchuen12@users.noreply.github.com>
2019-04-19 12:39:40 +0800

    Update main file.cpp

--- main file.cpp
+++ main file.cpp
@@ -23,45 +23,43 @@ void show_table() {
 }
 
 int main() {
-	int customer, all_available_table = 20, full_table = 0;
+	int customer = 0, all_available_table = 20, full_table = 0;
 	char YorN;
 	for (int i = 0; i < MAX_VALUE; i++) {
 		Table_Available[i] = 4;
 		Table_Occupied[i] = 0;
 	}
 
-	for (int i = 0; i < 20; i++) {
-		if (Table_Available[i] == 4) {
-			all_available_table += 1;
+	while (customer != -999) {
+		for (int i = 0; i < 20; i++) {
+			if (Table_Available[i] == 4) {
+				all_available_table += 1;
+			}
+			else if (Table_Available[i] == 0) {
+				full_table += 1;
+			}
 		}
-		else if (Table_Available[i] == 0) {
-			full_table += 1;
+		cout << "Number of available table: " << all_available_table << endl;
+		cout << "Number of full table: " << full_table << endl;
+		cout << "Number of available seat: " << 80 << endl;
+		cout << "Number of occupied seat: " << 0 << endl;
+		show_table();
+		cout << "Number of customer(s): ";
+		cin >> customer;
+		cout << "Recommendation(s) of table:" << endl;
+		for (int i = 0; i < 20; i++) {
+			if (customer <= Table_Available[i]) {
+				cout << (i + 1) << " ";
+			}
 		}
+		cout << endl;
+		for (int i = 0; i < 20; i++) {
+			if (customer <= Table_Available[i]) {
+				Table_Available[i] -= customer;
+				Table_Occupied[i] += customer;
+				break;
+			}
+		}
+		show_table();
 	}
-	cout << "Number of available table: " << all_available_table << endl;
-	cout << "Number of full table: " << full_table << endl;
-	cout << "Number of available seat: " << 80 << endl;
-	cout << "Number of occupied seat: " << 0 << endl;
-	show_table();
-	cout << "Number of customer(s): ";
-	cin >> customer;
-	cout << "Recommendation(s) of table:" << endl;
-	for (int i = 0; i < 20; i++) {
-		if (customer <= Table_Available[i]) {
-			cout << (i + 1) << " ";
-		}
-		else {
-			cout << "Separate into different group (Y/N): ";
-			cin >> YorN;
-		}
-	}
-	cout << endl;
-	for (int i = 0; i < 20; i++) {
-		if (customer <= Table_Available[i]) {
-			Table_Available[i] -= customer;
-			Table_Occupied[i] += customer;
-			break;
-		}
-	}
-	show_table();
 }
